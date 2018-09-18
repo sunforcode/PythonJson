c20b7d074e4d9b492fdd64f827df3821a2c218b2
sunyongji <363148025@qq.com>
2018-09-18 18:35:03 +0800

    修改获取字典

--- RootObjectPY.py
+++ RootObjectPY.py
@@ -1,5 +1,6 @@
 import json
 from workJson import jsonString
+
 #寻找根节点
 def getData(key,dic:dict):
     for k,v in dic.items():
@@ -7,6 +8,29 @@ def getData(key,dic:dict):
             return getData(key, v)
         else:
             return v
+
+#将整个param整合成一个字典
+def getParamsDic(rootKey,recurKey,dic):
+    resultDic:dict = {}
+    typeDic:dict = dic.get(rootKey) #获取到key为type的字典
+    for k,v in typeDic.items():
+        if k == recurKey and typeDic.get(recurKey) != None: #存在需要递归的字典,并且递归的字典不为None
+            tempDic = getData(recurKey, typeDic) #将递归的字典整合到最终的字典中
+            # print(tempDic)
+            for kt,vt in tempDic.items():
+                if kt != "ofType": #去掉这个递归的key
+                    # print(kt)
+                    # print(vt)
+                    resultDic[kt] = vt
+        else:
+            print(k)
+            resultDic[k] = v
+            pass
+        pass
+    return resultDic
+    pass
+
+
 
 rootJson = json.loads(jsonString)
 
@@ -22,9 +46,16 @@ for signleType in typesArray:
 
 fieldArray = signleType["fields"]
 # 找到了fields,fields是个list
+i = 0
 for signleField in fieldArray :
+    print(i)
+    i = i+1
     argsArray = signleField.get("args")
     for arg in argsArray:
-        type1 = arg["type"]  # dic
-        rootDic = getData("ofType",type1)
-        print(rootDic)
+       resultDic = getParamsDic("type","ofType",arg)
+       # print(resultDic)
+        # typeDic = arg["type"]  # dic
+        # rootType = getData("ofType",typeDic)
+        # print(i)
+        # print(rootType)
+        # i = i + 1
